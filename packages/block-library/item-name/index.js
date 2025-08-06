0ff8c3ddeffa43bdb31d639e24c493ff2f395b3d
JR Tashjian <hello@jrtashjian.com>
2025-08-05 22:07:36 -0400

    Add editor styles for item-name block

--- packages/block-library/item-name/index.js
+++ packages/block-library/item-name/index.js
@@ -10,6 +10,8 @@ import json from './block.json';
 import { default as edit } from './edit';
 import { default as variations } from './variations';
 
+import './index.scss';
+
 const { name } = json;
 
 registerBlockType( name, {
