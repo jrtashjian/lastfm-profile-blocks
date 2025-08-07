cc36b447428540c87f0adffe1e463f09ce4d698b
JR Tashjian <hello@jrtashjian.com>
2025-08-06 23:08:57 -0400

    Update TopCharts block

--- includes/BlockLibrary/Blocks/TopCharts.php
+++ includes/BlockLibrary/Blocks/TopCharts.php
@@ -7,6 +7,9 @@
 
 namespace ProfileBlocksLastFM\BlockLibrary\Blocks;
 
+use ProfileBlocksLastFM\Plugin\LastFM;
+use WP_Block;
+
 /**
  * The TopCharts block class.
  */
@@ -17,10 +20,35 @@ class TopCharts extends BaseBlock {
 	 * @return string Returns the block content.
 	 */
 	public function render() {
+		$method = match ( $this->get_block_attribute( 'chart' ) ) {
+			'albums'  => 'get_top_albums',
+			'artists' => 'get_top_artists',
+			'tracks'  => 'get_top_tracks',
+		};
+
+		$collection = LastFM::$method(
+			array(
+				'user'   => $this->get_block_attribute( 'user' ),
+				'period' => $this->get_block_attribute( 'period' ) ?? '7day',
+				'limit'  => $this->get_block_attribute( 'limit' ) ?? 6,
+			)
+		);
+
+		$this->content = '';
+
+		foreach ( $this->instance->parsed_block['innerBlocks'] as $block ) {
+			$block_instance = new WP_Block(
+				$block,
+				array( 'collection' => $collection )
+			);
+
+			$this->content .= $block_instance->render();
+		}
+
 		return sprintf(
 			'<div %s>%s</div>',
 			get_block_wrapper_attributes(),
-			self::class
+			$this->content
 		);
 	}
 }
