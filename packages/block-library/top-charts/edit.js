aa72297066c5a28828dca6a0219a9e55d6c9d6b0
JR Tashjian <hello@jrtashjian.com>
2025-08-07 18:05:31 -0400

    Add default templates for top artists, albums, and tracks in the top-charts block

--- packages/block-library/top-charts/edit.js
+++ packages/block-library/top-charts/edit.js
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import {
 	useBlockProps,
 	BlockContextProvider,
@@ -11,11 +12,49 @@ import { useEffect, useState } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 import { addQueryArgs } from '@wordpress/url';
 
-const TEMPLATE = [
-	[ 'profile-blocks-lastfm/dynamic-template', {}, [
-		[ 'profile-blocks-lastfm/item-image', { itemImageProp: 'album.images', itemLinkProp: 'album.url' } ],
-	] ],
-];
+const TEMPLATE = {
+	artists: [
+		[ 'core/heading', { level: 3, content: __( 'Top Artists', 'profile-blocks-lastfm' ) } ],
+		[ 'profile-blocks-lastfm/dynamic-template', { layout: { type: 'flex' } }, [
+			[ 'profile-blocks-lastfm/item-image', { itemImageProp: 'artist.images', itemLinkProp: 'artist.url' } ],
+			[ 'core/group', { layout: { type: 'flex', flexWrap: 'nowrap', orientation: 'vertical' }, style: { layout: { selfStretch: 'fill' }, spacing: { blockGap: '0' } } }, [
+				[ 'profile-blocks-lastfm/item-name', { itemTextProp: 'artist.name', itemLinkProp: 'artist.url', isLink: true, style: { typography: { fontStyle: 'normal', fontWeight: 700 } } } ],
+			] ],
+			[ 'core/group', { layout: { type: 'flex', flexWrap: 'nowrap' }, style: { spacing: { blockGap: '0.25em' }, typography: { fontSize: '0.8em' } } }, [
+				[ 'profile-blocks-lastfm/item-name', { itemTextProp: 'artist.playcount' } ],
+				[ 'core/paragraph', { content: __( 'plays', 'profile-blocks-lastfm' ) } ],
+			] ],
+		] ],
+	],
+	albums: [
+		[ 'core/heading', { level: 3, content: __( 'Top Albums', 'profile-blocks-lastfm' ) } ],
+		[ 'profile-blocks-lastfm/dynamic-template', { layout: { type: 'flex' } }, [
+			[ 'profile-blocks-lastfm/item-image', { itemImageProp: 'album.images', itemLinkProp: 'album.url' } ],
+			[ 'core/group', { layout: { type: 'flex', flexWrap: 'nowrap', orientation: 'vertical' }, style: { layout: { selfStretch: 'fill' }, spacing: { blockGap: '0' } } }, [
+				[ 'profile-blocks-lastfm/item-name', { itemTextProp: 'album.name', itemLinkProp: 'album.url', isLink: true, style: { typography: { fontStyle: 'normal', fontWeight: 700 } } } ],
+				[ 'profile-blocks-lastfm/item-name', { itemTextProp: 'artist.name', itemLinkProp: 'artist.url', isLink: true, style: { typography: { fontSize: '0.8em' } } } ],
+			] ],
+			[ 'core/group', { layout: { type: 'flex', flexWrap: 'nowrap' }, style: { spacing: { blockGap: '0.25em' }, typography: { fontSize: '0.8em' } } }, [
+				[ 'profile-blocks-lastfm/item-name', { itemTextProp: 'album.playcount' } ],
+				[ 'core/paragraph', { content: __( 'plays', 'profile-blocks-lastfm' ) } ],
+			] ],
+		] ],
+	],
+	tracks: [
+		[ 'core/heading', { level: 3, content: __( 'Top Tracks', 'profile-blocks-lastfm' ) } ],
+		[ 'profile-blocks-lastfm/dynamic-template', { layout: { type: 'flex' } }, [
+			[ 'profile-blocks-lastfm/item-image', { itemImageProp: 'track.images', itemLinkProp: 'track.url' } ],
+			[ 'core/group', { layout: { type: 'flex', flexWrap: 'nowrap', orientation: 'vertical' }, style: { layout: { selfStretch: 'fill' }, spacing: { blockGap: '0' } } }, [
+				[ 'profile-blocks-lastfm/item-name', { itemTextProp: 'track.name', itemLinkProp: 'track.url', isLink: true, style: { typography: { fontStyle: 'normal', fontWeight: 700 } } } ],
+				[ 'profile-blocks-lastfm/item-name', { itemTextProp: 'artist.name', itemLinkProp: 'artist.url', isLink: true, style: { typography: { fontSize: '0.8em' } } } ],
+			] ],
+			[ 'core/group', { layout: { type: 'flex', flexWrap: 'nowrap' }, style: { spacing: { blockGap: '0.25em' }, typography: { fontSize: '0.8em' } } }, [
+				[ 'profile-blocks-lastfm/item-name', { itemTextProp: 'track.playcount' } ],
+				[ 'core/paragraph', { content: __( 'plays', 'profile-blocks-lastfm' ) } ],
+			] ],
+		] ],
+	],
+};
 
 const Edit = ( {
 	attributes: { chart },
@@ -54,7 +93,7 @@ const Edit = ( {
 	}, [ apiKey, defaultProfile ] );
 
 	const blockProps = useBlockProps();
-	const innerBlockProps = useInnerBlocksProps( blockProps, { template: TEMPLATE } );
+	const innerBlockProps = useInnerBlocksProps( blockProps, { template: TEMPLATE[ chart ] } );
 
 	return (
 		<BlockContextProvider value={ { collection } }>
