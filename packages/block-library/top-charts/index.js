da31ae6ab1cd0e3178ec526f24c1b9842efb3632
JR Tashjian <hello@jrtashjian.com>
2025-08-20 20:32:29 -0400

    lint fixes

--- packages/block-library/top-charts/index.js
+++ packages/block-library/top-charts/index.js
@@ -11,7 +11,6 @@ import json from './block.json';
 import { default as edit } from './edit';
 import { default as variations } from './variations';
 
-import './style.scss';
 import './index.scss';
 
 const { name } = json;
